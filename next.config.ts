3980b1ff0791fde2ad9cabe4e6bd278088a8704a
nzilla <victorbliss044@gmail.com>
2025-07-08 17:21:05 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: false, 
+  },
 };
 
+
 export default nextConfig;
+
